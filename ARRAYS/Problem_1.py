f991453125666f4f435c38b62e85be1358baba89
Narasimha <satyansv5@gmail.com>
2026-01-11 12:37:04 +0530

    Two Sum

---
+++ ARRAYS/Problem_1.py
@@ -0,0 +1,15 @@
+# Array Problem 1 — Two Sum
+# 🧩 Problem Statement
+
+# Given an array nums and a target target, return indices of the two numbers such that they add up to target.
+# You may assume exactly one solution, and you cannot use the same element twice.
+
+# Approach — Brute Force (O(n²))
+
+class Solution:
+    def twoSum(self, nums, target):
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
